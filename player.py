2da8b0bb4071e299820fac3a75881d6ce90355ca
Saul Ruiz <saulruiz730@gmail.com>
2022-09-29 11:44:35 -0500

    added user validation for gesture selection

--- player.py
+++ player.py
@@ -9,8 +9,13 @@ class Player:
         for gesture in self.gestures:
             i += 1
             print(f'[{i}] {gesture}')     
-        gesture_selection = int(input(f'Select your move: '))
+        gesture_selection = input(f'Select your move: ')
 
+        while gesture_selection != '1' and gesture_selection != '2' and gesture_selection != '3' and gesture_selection != '4' and gesture_selection != '5':
+            print('Invalid')
+            gesture_selection = input(f'Select your move: ')
+
+        gesture_selection = int(gesture_selection)
         gesture_index = gesture_selection - 1
         gesture = self.gestures[gesture_index]
         print(f'{player_name} plays: {gesture}!')
